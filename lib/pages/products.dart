1c48d4ca8a12d600b6cccfe9242a28f58964f41b
hukoru+1-at-580984682628 <N0gGUYPyj1gWPrX7JSuwIBAOuWDl7F++GSPmIr3zuhc=>
2019-01-02 15:47:31 +0900

    Adding Left Menu

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -5,6 +5,20 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Prodcts'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('커피 리스트'),
       ),
