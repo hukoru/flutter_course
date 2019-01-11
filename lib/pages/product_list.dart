270eafcfaddead5cfb868afa10eb8cf968e957ab
hukoru+1-at-580984682628 <N0gGUYPyj1gWPrX7JSuwIBAOuWDl7F++GSPmIr3zuhc=>
2019-01-11 13:57:21 +0900

    Improving structure

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -4,39 +4,58 @@ import './product_edit.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function updateProduct;
+  final Function deleteProduct;
   final List<Map<String, dynamic>> products;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
+
+  Widget _buildEditButton(BuildContext context, int index) {
+    return IconButton(
+      icon: Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) {
+              return ProductEditPage(
+                product: products[index],
+                updateProduct: updateProduct,
+                productIndex: index,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return Column(children: <Widget>[
-          ListTile(
-            leading: CircleAvatar(
-                backgroundImage: AssetImage(products[index]['image'])),
-            title: Text(products[index]['title']),
-            subtitle: Text('\$${products[index]['price'].toString()}'),
-            trailing: IconButton(
-              icon: Icon(Icons.edit),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
+        return Dismissible(
+          key: Key(products[index]['title']),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            } else if (direction == DismissDirection.startToEnd) {
+              print('스와이프 start ==> end');
+            } else {
+              print('다른 스와이핑');
+            }
+          },
+          background: Container(
+            color: Colors.red,
           ),
-          Divider(),
-        ]);
+          child: Column(children: <Widget>[
+            ListTile(
+                leading: CircleAvatar(
+                    backgroundImage: AssetImage(products[index]['image'])),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price'].toString()}'),
+                trailing: _buildEditButton(context, index)),
+            Divider(),
+          ]),
+        );
       },
       itemCount: products.length,
     );
